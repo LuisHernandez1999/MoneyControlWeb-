ec36a93ffc6e2860befaa6095949c4561d46340c
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-01-16 19:35:54 -0300

    desing login novo

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -23,7 +23,7 @@ const ResetPassword = () => {
             >
                 <Box
                     sx={{
-                        backgroundImage: 'url(/imagens/bak.png)',
+                        backgroundColor: '#f0f0f0', // Fundo cinza claro
                         backgroundSize: 'cover',
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'center',
@@ -43,16 +43,16 @@ const ResetPassword = () => {
                         height: '70vh',
                         zIndex: 1,
                         border: '1px solid rgba(0, 0, 0, 0.1)', 
-                        borderRadius: '12px',
+                        borderRadius: '24px',
                         boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)', 
                     }}
                 >
                     {/* Lado esquerdo com imagem */}
                     <Box
                         sx={{
-                            backgroundColor: 'black',
+                            backgroundColor: '#333',
                             flex: 1,
-                            borderRadius: '12px 0 0 12px', 
+                            borderRadius: '24px 0 0 24px', 
                             display: 'flex',
                             justifyContent: 'center',
                             alignItems: 'center',
@@ -72,11 +72,39 @@ const ResetPassword = () => {
                             bottom: 0 
                         }}>
                             <Image 
-                                src="/imagens/money3.jpg" // Caminho da imagem
+                                src="/imagens/img5.png" // Caminho da imagem
                                 alt="Logo"
                                 layout="fill" // Preenche toda a área disponível
                                 objectFit="cover" // Faz a imagem cobrir toda a área
                             />
+                              <Box
+        sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fundo preto semitransparente
+            backdropFilter: 'blur(2px)', // Reduzido o desfoque
+            borderRadius: '12px 0 0 12px',// Mesmo arredondamento do container
+        }}
+    />
+     <Typography
+        variant="h4"
+        sx={{
+            position: 'relative', // Para manter sobre o blur
+            color: 'white',
+            fontWeight: 'bold',
+            zIndex: 2, // Para garantir que o texto fique acima do blur
+            textAlign: 'center',
+            padding: '1rem',
+            textShadow: '0px 0px 10px rgba(0, 0, 0, 1)', // Adiciona um leve contorno ao texto
+            paddingTop: '14rem', 
+        }}
+    >
+        Bem-vindo ao Virtual Money Vault, alcance o controle do seu dinheiro 
+    </Typography>
+
                         </Box>
                     </Box>
                     
@@ -90,15 +118,30 @@ const ResetPassword = () => {
                             flexDirection: 'column',
                             justifyContent: 'flex-start',
                             alignItems: 'center',
-                            paddingTop: '9rem', 
+                            paddingTop: '3rem', 
                         }}
                     >
+                        <Box
+        sx={{
+            position: 'relative',
+            width: 200, // Largura do logotipo
+            height: 190, // Altura do logotipo
+            marginBottom: '1rem', // Espaço entre o logotipo e o texto
+        }}
+    >
+        <Image
+            src="/imagens/moneylogo2.png" // Caminho da imagem do logotipo
+            alt="Logotipo"
+            layout="fill" // Faz a imagem ocupar toda a área do contêiner
+            objectFit="cover" // Ajusta a imagem para cobrir o contêiner
+        />
+    </Box>
                        
                         <Typography
                             variant="h6"
                             sx={{
                                 marginBottom: '2rem', 
-                                color: '#1DB954',
+                                color: '#black',
                                 fontFamily: "'Plus Jakarta Sans', sans-serif",
                                 fontWeight: 'bold',
                                 fontSize: '35px',
@@ -142,11 +185,12 @@ const ResetPassword = () => {
                         <Button
                             variant="contained"
                             sx={{
-                                width: '20%',
+                                width: '17%',
                                 padding: '0.2rem 1rem', 
                                 height: '40px', 
-                                color: 'white',
-                                backgroundColor: 'black',
+                                color: 'black',
+                                border: '2px solid black',
+                                backgroundColor: 'white',
                                 fontSize: '14px',
                                 borderRadius: '25px',
                                 '&:hover': {
@@ -163,7 +207,7 @@ const ResetPassword = () => {
                                 fontSize: '1rem',
                                 color: 'black',
                                 marginTop: '0.9rem', 
-                                textDecoration: 'underline',
+                               
                                 cursor: 'pointer',
                                 '&:hover': {
                                     color: 'gray',
